1683c3f2ee71c19ad86a6e6566c33f4d67d3714a
repodevs <repopamor@gmail.com>
2015-12-31 15:41:31 +0700

    No first commit without Bismillah

---
+++ newmodule/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Belajar Module Odoo',
+    'version': '1.0',
+    'author': 'Edi',
+    'complexity': 'normal',
+    'description': 'Belajar membuat Module pada odoo',
+    'category': '',
+    'depends': [],
+    'data': [
+    	'views/mahasiswa_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+    },
+}
