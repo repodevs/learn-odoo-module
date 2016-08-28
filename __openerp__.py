6006290db520f5f78b3fce9fc2d3c3dcb6840404
Edi Santoso <repopamor@gmail.com>
2016-08-28 22:59:07 +0700

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Book",
+    'summary':  "Short subtitle phrase",
+    'description':  """Long description""",
+    'author':   "Edi Santoso - REVSTECH",
+    'license':  "AGPL-3",
+    'website':  "http://about.me/esantoso",
+    'category': "Uncategorized",
+    'version':  '9.0.1.0.0',
+    'depends':  ['base'],
+    'data': [
+                'views/library_book.xml',
+            ],
+    'demo': [
+                # 'demo.xml'
+            ],
+}
---
+++ models/library_book.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book' # membuat deskripsi di database
+    _order = 'date_release desc, name' #membuat urutan berdasarkan date_release kemudian name
+    _rec_name = 'short_name'
+
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title')
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many('res.partner', string='Authors')
+
+
+    # def name_get(self):
+    #     """
+    #     untuk mendefinisikan name_get
+    #     title pada atas halaman ketika saat form view
+    #     """
+    #     result = []
+    #     for record in self:
+    #         result.append(
+    #             (record.id,
+    #             u"%s (%s)" % (record.name, record.date_release)
+    #         ))
+    #     return result
