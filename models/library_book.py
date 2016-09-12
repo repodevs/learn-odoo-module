aa7eb3cb3afe6218547b78d60c7fff8a39b3ba57
Edi Santoso <repopamor@gmail.com>
2016-09-12 20:27:59 +0700

    Adding features to a Model using inheritance

--- models/library_book.py
+++ models/library_book.py
@@ -7,6 +7,7 @@ from datetime import timedelta as td
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    _order = 'name'
     book_ids = fields.One2many(
         'library.book', 'publisher_id',
         string='Published Books'
@@ -16,6 +17,21 @@ class ResPartner(models.Model):
         string='Authored Books',
         # relation='library_book_res_partner_rel'  # optional
         )
+    authored_book_ids = fields.Many2many(
+        'library.book', string='Authored Books'
+        )
+    count_books = fields.Integer(
+        'Number of Authored Books',
+        compute='_compute_count_books'
+        )
+
+
+    # add the method needed to compute the book count
+    @api.depends('authored_book_ids')
+    def _compute_count_books(self):
+        for r in self:
+            r.count_books = len(r.authored_book_ids)
+
 
 
 class LibraryBook(models.Model):
