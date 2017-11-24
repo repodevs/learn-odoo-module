047c72ce2b24b60cf1a825bb7d81327b9a50bb11
Edi Santoso <repopamor@gmail.com>
2017-11-24 07:09:33 +0700

    CH3: Init Create Module

---
+++ my_module/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My module",
+    'summary': "Short Subtitle phrase",
+    'description': """Long Description""",
+    'author': "Edi Santoso",
+    'license': "AGPL-3",
+    'website': "https://me.repodevs.com",
+    'category': 'Uncategorized',
+    'version': '9.0.1.0.0',
+    'depends': ['base'],
+    'data': ['views.xml'],
+    'demo': ['demo.xml'],
+}
+
