6e40a14019d28dd648286552daa201e4bb5a9068
Edi Santoso <repopamor@gmail.com>
2017-12-12 16:44:13 +0700

    CH4: Defining the Model representation and order

--- my_module/models/library_book.py
+++ my_module/models/library_book.py
@@ -4,8 +4,11 @@ from openerp import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
+
     name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title')
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-
-
